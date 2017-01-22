71cf486be3a2c1df7784bfaa8da62d04234136c0
Gary Miller <gamiller286@gmail.com>
2017-01-22 14:02:45 -0600

    Completed first part of tutorial
    Placing game objects, spawning enemies, player/enemy movement,
    gameManager script for keeping track of money etc. , HealthBar
    indicator...

---
+++ Assets/PlaceMonster.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMonster : MonoBehaviour {
+
+    public GameObject monsterPrefab;
+    private GameObject monster;
+
+    private GameManagerBehavior gameManager;
+
+    private bool canPlaceMonster()
+    {
+        int cost = monsterPrefab.GetComponent<MonsterData>().levels[0].cost;
+        return monster == null && gameManager.Gold >= cost;
+    }
+
+    private bool canUpgradeMonster()
+    {
+        if (monster != null)
+        {
+            MonsterData monsterData = monster.GetComponent<MonsterData>();
+            MonsterData.MonsterLevel nextLevel = monsterData.getNextLevel();
+            if (nextLevel != null)
+            {
+                return gameManager.Gold >= nextLevel.cost; ;
+            }
+        }
+        return false;
+    }
+
+    void OnMouseUp()
+    {
+        if (canPlaceMonster())
+        {
+            monster = (GameObject)Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+        }
+        else if(canUpgradeMonster())
+        {
+            monster.GetComponent<MonsterData>().increaseLevel();
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+        }
+    }
+    
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
